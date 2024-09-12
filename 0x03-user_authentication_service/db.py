874e951e1cb6fefdca5e83f99ebd23d53dc602b8
ronsig30 <ronaldrogony30@gmail.com>
2024-09-12 20:32:08 +0300

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,40 +2,32 @@
 """
 DB module
 """
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound as NoResultFoundLegacy
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class to manage the database"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-        """
+        """Adds a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
@@ -46,9 +38,9 @@ class DB:
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound("No user found for given criteria")
+                raise NoResultFound
             return user
-        except NoResultFoundLegacy:
+        except NoResultFound:
             raise NoResultFound("No user found")
         except InvalidRequestError:
             raise InvalidRequestError("Invalid query arguments")
